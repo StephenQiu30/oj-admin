62e63f4feadea2be7704c05e934f55f346bcb075
StephenQiu30 <Popcornqhd@gmail.com>
2025-12-27 18:53:25 +0800

    feat:添加关于题目的api

--- src/services/oj-backend/index.ts
+++ src/services/oj-backend/index.ts
@@ -3,8 +3,10 @@
 // API 更新时间：
 // API 唯一标识：
 import * as userController from './userController';
+import * as questionController from './questionController';
 import * as fileController from './fileController';
 export default {
   userController,
+  questionController,
   fileController,
 };
--- src/services/oj-backend/typings.d.ts
+++ src/services/oj-backend/typings.d.ts
@@ -25,6 +25,22 @@ declare namespace API {
     message?: string;
   };
 
+  type BaseResponsePageQuestion = {
+    /** 状态码 */
+    code?: number;
+    data?: PageQuestion;
+    /** 消息 */
+    message?: string;
+  };
+
+  type BaseResponsePageQuestionVO = {
+    /** 状态码 */
+    code?: number;
+    data?: PageQuestionVO;
+    /** 消息 */
+    message?: string;
+  };
+
   type BaseResponsePageUser = {
     /** 状态码 */
     code?: number;
@@ -37,6 +53,14 @@ declare namespace API {
     /** 状态码 */
     code?: number;
     data?: PageUserVO;
+    /** 消息 */
+    message?: string;
+  };
+
+  type BaseResponseQuestionVO = {
+    /** 状态码 */
+    code?: number;
+    data?: QuestionVO;
     /** 消息 */
     message?: string;
   };
@@ -71,12 +95,32 @@ declare namespace API {
     id?: number;
   };
 
+  type getQuestionVOByIdParams = {
+    id: number;
+  };
+
   type getUserByIdParams = {
     id: number;
   };
 
   type getUserVOByIdParams = {
     id: number;
+  };
+
+  type JudgeCase = {
+    /** 输入用例 */
+    input?: string;
+    /** 输出用例 */
+    output?: string;
+  };
+
+  type JudgeConfig = {
+    /** 时间限制（ms） */
+    timeLimit?: number;
+    /** 内存限制（KB） */
+    memoryLimit?: number;
+    /** 堆栈限制（KB） */
+    stackLimit?: number;
   };
 
   type LoginUserVO = {
@@ -101,6 +145,34 @@ declare namespace API {
   type OrderItem = {
     column?: string;
     asc?: boolean;
+  };
+
+  type PageQuestion = {
+    records?: Question[];
+    total?: number;
+    size?: number;
+    current?: number;
+    orders?: OrderItem[];
+    optimizeCountSql?: PageQuestion;
+    searchCount?: PageQuestion;
+    optimizeJoinOfCountSql?: boolean;
+    maxLimit?: number;
+    countId?: string;
+    pages?: number;
+  };
+
+  type PageQuestionVO = {
+    records?: QuestionVO[];
+    total?: number;
+    size?: number;
+    current?: number;
+    orders?: OrderItem[];
+    optimizeCountSql?: PageQuestionVO;
+    searchCount?: PageQuestionVO;
+    optimizeJoinOfCountSql?: boolean;
+    maxLimit?: number;
+    countId?: string;
+    pages?: number;
   };
 
   type PageUser = {
@@ -131,6 +203,155 @@ declare namespace API {
     pages?: number;
   };
 
+  type Question = {
+    /** id */
+    id?: number;
+    /** 标题 */
+    title?: string;
+    /** 内容 */
+    content?: string;
+    /** 标签(json数组) */
+    tags?: string;
+    /** 答案 */
+    answer?: string;
+    /** 题目提交数 */
+    submitNum?: number;
+    /** 题目通过数 */
+    acceptNum?: number;
+    /** 判题用例(json数组) */
+    judgeCase?: string;
+    /** 判题配置(json数组) */
+    judgeConfig?: string;
+    /** 题目点赞数 */
+    thumbNum?: number;
+    /** 题目收藏数 */
+    favourNum?: number;
+    /** 创建用户id */
+    userId?: number;
+    /** 创建时间 */
+    createTime?: string;
+    /** 更新时间 */
+    updateTime?: string;
+    /** 是否删除 */
+    isDelete?: number;
+  };
+
+  type QuestionAddRequest = {
+    /** 标题 */
+    title?: string;
+    /** 内容 */
+    content?: string;
+    /** 标签(json数组) */
+    tags?: string[];
+    /** 答案 */
+    answer?: string;
+    /** 判题用例(json数组) */
+    judgeCase?: JudgeCase[];
+    /** 判题配置(json数组) */
+    judgeConfig?: JudgeConfig[];
+  };
+
+  type QuestionEditRequest = {
+    /** id */
+    id?: number;
+    /** 标题 */
+    title?: string;
+    /** 内容 */
+    content?: string;
+    /** 标签(json数组) */
+    tags?: string[];
+    /** 答案 */
+    answer?: string;
+    /** 判题用例(json数组) */
+    judgeCase?: JudgeCase[];
+    /** 判题配置(json数组) */
+    judgeConfig?: JudgeConfig[];
+  };
+
+  type QuestionQueryRequest = {
+    /** 当前页号 */
+    current?: number;
+    /** 页面大小 */
+    pageSize?: number;
+    /** 排序字段 */
+    sortField?: string;
+    /** 排序顺序（默认升序） */
+    sortOrder?: string;
+    /** id */
+    id?: number;
+    /** notId */
+    notId?: number;
+    /** 搜索词 */
+    searchText?: string;
+    /** 标题 */
+    title?: string;
+    /** 内容 */
+    content?: string;
+    /** 标签(json数组) */
+    tags?: string[];
+    /** 答案 */
+    answer?: string;
+    /** 创建用户id */
+    userId?: number;
+  };
+
+  type QuestionUpdateRequest = {
+    /** id */
+    id?: number;
+    /** 标题 */
+    title?: string;
+    /** 内容 */
+    content?: string;
+    /** 标签(json数组) */
+    tags?: string[];
+    /** 答案 */
+    answer?: string;
+    /** 题目提交数 */
+    submitNum?: number;
+    /** 题目通过数 */
+    acceptNum?: number;
+    /** 判题用例(json数组) */
+    judgeCase?: JudgeCase[];
+    /** 判题配置(json数组) */
+    judgeConfig?: JudgeConfig[];
+    /** 题目点赞数 */
+    thumbNum?: number;
+    /** 题目收藏数 */
+    favourNum?: number;
+  };
+
+  type QuestionVO = {
+    /** id */
+    id?: number;
+    /** 标题 */
+    title?: string;
+    /** 内容 */
+    content?: string;
+    /** 标签(json数组) */
+    tags?: string[];
+    /** 答案 */
+    answer?: string;
+    /** 题目提交数 */
+    submitNum?: number;
+    /** 题目通过数 */
+    acceptNum?: number;
+    /** 判题用例(json数组) */
+    judgeCase?: JudgeCase[];
+    /** 判题配置(json数组) */
+    judgeConfig?: JudgeConfig[];
+    /** 题目点赞数 */
+    thumbNum?: number;
+    /** 题目收藏数 */
+    favourNum?: number;
+    /** 用户id */
+    userId?: number;
+    /** 创建时间 */
+    createTime?: string;
+    /** 更新时间 */
+    updateTime?: string;
+    userVO?: UserVO;
+  };
+
   type uploadFileParams = {
     uploadFileRequest: UploadFileRequest;
   };
@@ -147,18 +368,14 @@ declare namespace API {
     userAccount?: string;
     /** 用户密码 */
     userPassword?: string;
-    /** 开放平台id */
-    unionId?: string;
-    /** 公众号openId */
-    mpOpenId?: string;
     /** 用户昵称 */
     userName?: string;
+    /** 用户邮箱 */
+    userEmail?: string;
     /** 用户头像 */
     userAvatar?: string;
     /** 用户角色：user/admin/ban */
     userRole?: string;
-    /** 用户邮箱 */
-    userEmail?: string;
     /** 创建时间 */
     createTime?: string;
     /** 更新时间 */
@@ -254,10 +471,6 @@ declare namespace API {
     id?: number;
     /** 用户昵称 */
     userName?: string;
-    /** 开放平台id */
-    unionId?: string;
-    /** 公众号openId */
-    mpOpenId?: string;
     /** 用户头像 */
     userAvatar?: string;
     /** 用户角色：user/admin/ban */
